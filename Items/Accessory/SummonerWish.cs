5135af829bf56e7cf11450571517911b6399249e
TMC <cgamez995@gmail.com>
2017-11-21 20:46:27 -0800

    Fixed tooltip issue

--- Items/Accessory/SummonerWish.cs
+++ Items/Accessory/SummonerWish.cs
@@ -10,26 +10,23 @@ namespace TheHeroIeralian.Items.Accessory
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Summoner's Wish");
-			Tooltip.SetDefault("Increases max amount of minions by 3");
-            Tooltip.SetDefault("Increases minion damage by 18%");
-            Tooltip.SetDefault("Increases minion knockback by 16%");
+			Tooltip.SetDefault("Increases max amount of minions by 3\nIncreases minion damage by 18%\nIncreases minion knockback by 16%");
+        	}
 
-        }
-
-        public override void SetDefaults()
+        	public override void SetDefaults()
 		{
 			item.width = 34;
 			item.height = 34;
-            item.value = Item.buyPrice(0, 13, 86, 0);
-            item.rare = 9;
-            item.accessory = true;
-        }
+            		item.value = Item.buyPrice(0, 13, 86, 0);
+            		item.rare = 9;
+            		item.accessory = true;
+        	}
 
-        public override void UpdateAccessory(Player player, bool hideVisual)
-        {
-            player.maxMinions += 3;
-            player.minionDamage = player.minionDamage *1.18f;
-            player.minionKB = player.minionKB * 1.16f;
-        }
-    }
+        	public override void UpdateAccessory(Player player, bool hideVisual)
+        	{
+            		player.maxMinions += 3;
+            		player.minionDamage = player.minionDamage *1.18f;
+            		player.minionKB = player.minionKB * 1.16f;
+        	}
+    	}
 }
